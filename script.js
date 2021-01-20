e8dff17b862c709978385b0f1c044b8b616d1dae
Steve Gekas <75593974+SteveGekas@users.noreply.github.com>
2021-01-19 22:26:28 -0500

    added questions for quiz

--- script.js
+++ script.js
@@ -1,4 +1,49 @@
-var questions = 0;
+var quiz = [
+    {
+        question: "Which of the following is not JavaScript Data Types?",
+        choices: ["Undefined", "Number", "Boolean", "Float"],
+        correct: "Float"
+    },
+
+    {
+        question: "Which company developed JavaScript?",
+        choices: ["Netscape", "Bell Labs", "Sun Microsystems", "IBM"],
+        correct: "Netscape"
+    },
+
+    {
+        question: "Inside which HTML element do we put the JavaScript?",
+        choices: ["<script>", "<head>", "<meta>", "<style>"],
+        correct: "<script>"
+    },
+
+    {
+        question: "What is the original name of JavaScript?",
+        choices: ["LiveScript", "EScript", "Mocha", "JavaScrip"],
+        correct: "Mocha"
+    },
+
+    {
+        question: "Which of them is not the looping structures in JavaScript?",
+        choices: ["for", "while", "forwhich", "dowhile"],
+        correct: "forwhich"
+    },
+
+    {
+        question: "What are the types of Pop up boxes available in JavaScript?",
+        choices: ["Alert", "Prompt", "Confirm", "All the Above"],
+        correct: "All the Above"
+    },
+
+    {
+        question: "Javascript string using double quotes is exactly the same as a string using single quotes?",
+        choices: ["Never", "Always", "Sometimes", "Rarely"],
+        correct: "Always"
+    }
+];
+
+
+var questionsAsked = 0;
 var score = 0;
 var data = 0;
 var currentTime = document.querySelector("#start");
