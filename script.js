22566da25cc17e8c326114f234bae1ba05a86563
Steve Gekas <75593974+SteveGekas@users.noreply.github.com>
2021-01-19 21:50:47 -0500

    got timer to work

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-var 
+var questions = 0;
+var score = 0;
+var data = 0;
+var currentTime = document.querySelector("#start");
+var timer = document.querySelector("#currentTime");
+var seconds = 61;
+var done = 0;
+
+
+currentTime.addEventListener("click", function () {
+    //alert("WORK PLEASE");
+    if (done === 0) {
+        holdInterval = setInterval(function () {
+            seconds--;
+            timer.textContent = "Time: " + seconds;
+
+            if (seconds <= 0) {
+                clearInterval(done);
+                timer.textContent = "GAME OVER";
+            }
+        }, 1000);
+    }
+});
